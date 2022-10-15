2fbb090b32491633d3ed63142553936f6136b130
Tony Grimes <acidtone@tonygrimes.com>
2022-10-15 16:12:18 -0600

    moved all the stuff to tempalte

--- src/lib/components/DiceRoller.svelte
+++ src/lib/components/DiceRoller.svelte
@@ -1,133 +1,98 @@
 <script>
-  import { onMount } from 'svelte';
 	import { roll,reduceRollResults } from '$lib/game/game-kit.js';
 	import { dieFaces, dice } from '$lib/game/game.js';
-
+  
   let rollBtn;
   let resolveBtn
   let resetBtn;
-
+  
   let rollPile;
   let keepPile;
   let resolvePile;
-
+  
   let rollState = 'initial'; // initial|rolling|resolved
+  let rollCount = 0;
 
-  // const toggleKeep = event => {
-  //   const dieId = parseInt(event.target.getAttribute('data-id'));
-  //   const die = $dice.find(item => item.id === dieId);
-  //   die.keep = !die.keep;  
-  //   dice = dice;
-  // }
-
+  let rollResults = [];
 
   const resolveDice = () => {
 
-    // Remove click handlers from pile Dice
-    const pileDice = document.querySelectorAll('button.die');
-
-    pileDice.forEach((item) => {
-      item.setAttribute('disabled', '');
-    })
-
     // Reduce the results of the dice
-    const rollResults = reduceRollResults($dice);
-
-    // Take reduced dice results and output to the page
-    let keptValuesDisplay = '';
-    for(let faceValue in rollResults){
-        keptValuesDisplay += `<li><button data-keep="">${faceValue} ${rollResults[faceValue]}</button></li>` 
-    }
-    resolvePile.innerHTML = keptValuesDisplay; 
+    rollResults = reduceRollResults($dice);
+ 
     rollState = 'resolved';
 
   }
 
   const resetDice = () => {
-    dice.forEach((die) => {
-      die.value = '';
-      die.keep = false;
+    $dice.forEach((die, index) => {
+      $dice[index].value = '';
+      $dice[index].keep = false;
     });
     
     rollCount = 0;
-
+    rollResults = [];
     rollState = 'initial';
-
-    rollPile.innerHTML = '';
-    keepPile.innerHTML = '';
-    resolvePile.innerHTML = '';
   }
 
+  const rollDice = () => {
 
-	onMount(async () => {
-  
-    // Dice
-    // TODO: Move renderPiles to game.js file?
+    rollState = 'rolling';
 
-  
-  
-    let rollCount = 0;
-
-    // TODO: move to template
-    rollBtn.addEventListener('click', () => {
-
-      rollState = 'rolling';
-
-      if (rollCount < 3) {
-        $dice.forEach((die, index) => {
-          if (!die.keep) {
-            $dice[index].value = roll(dieFaces).label;
-          }
-        })
-    
-        rollCount++;
-
-        if (rollCount === 3) {
-          resolveDice();
+    if (rollCount < 3) {
+      $dice.forEach((die, index) => {
+        if (!die.keep) {
+          $dice[index].value = roll(dieFaces).label;
         }
+      })
 
-      } else {
+      rollCount++;
+
+      if (rollCount === 3) {
         resolveDice();
       }
 
-    })
+    } else {
+      resolveDice();
+    }
 
-    // TODO: move to template
-    resetBtn.addEventListener('click', resetDice);
-    resolveBtn.addEventListener('click', resolveDice);
+  };
 
-  }); 
-  $: resolveDisabled = rollState==='initial' || rollState==='resolved';
+  $: resolveDisabled = rollState!=='rolling';
   $: rollDisabled = rollState==='resolved';
+  $: resetDisabled = rollState==='initial';
 
 </script>
 
 <div class="dice">
 	<section class="roll-nav">
 		<ul>
-			<li><button bind:this={rollBtn} class="roll-dice" disabled={rollDisabled}>Roll Dice</button></li>
-			<li><button bind:this={resolveBtn} class="resolve-dice" disabled={resolveDisabled}>Resolve</button></li>
-			<li><button bind:this={resetBtn} class="reset-dice">Reset</button></li>
+			<li><button bind:this={rollBtn} on:click={rollDice} class="roll-dice" disabled={rollDisabled}>Roll Dice</button></li>
+			<li><button bind:this={resolveBtn} on:click={resolveDice} class="resolve-dice" disabled={resolveDisabled}>Resolve</button></li>
+			<li><button bind:this={resetBtn} on:click={resetDice} class="reset-dice" disabled={resetDisabled}>Reset</button></li>
 		</ul>
 	</section>
 	<section class="dice-piles">
     <ul bind:this={keepPile} class="keep-pile">
       {#each $dice as die, index}
         {#if die.keep}
-          <li><button on:click={() => $dice[index].keep = !die.keep} class="die {die.value}" aria-label={die.value}></button></li>
+          <li><button on:click={() => $dice[index].keep = !die.keep} class="die {die.value}" aria-label={die.value} disabled={resolveDisabled}></button></li>
         {/if}
       {/each}
 		</ul>
 		<ul bind:this={rollPile} class="roll-pile">
       {#each $dice as die, index}
         {#if !die.keep}
-          <li><button on:click={() => $dice[index].keep = !die.keep} class="die {die.value}" aria-label={die.value}></button></li>
+          <li><button on:click={() => $dice[index].keep = !die.keep} class="die {die.value}" aria-label={die.value} disabled={resolveDisabled}></button></li>
         {/if}
       {/each}
 		</ul>
 	</section>
 	<section bind:this={resolvePile} class="resolve-pile">
 		<ul>
+      {#each Object.entries(rollResults) as [key, value]}
+        <li><button disabled>{key}: {value}</button></li>
+      {/each}
 		</ul>
 	</section>
 </div>
