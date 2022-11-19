72e5441af5ba90c9cbadddd65a3ef054bd7fdb2b
Tony Grimes <acidtone@tonygrimes.com>
2022-11-19 13:54:45 -0700

    tweak

--- src/lib/components/PowerCard.svelte
+++ src/lib/components/PowerCard.svelte
@@ -2,12 +2,10 @@
   export let card;
   export let onClick = null;
 
-  const meClicky = (e) => onClick(e);
-
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<article data-id="{card.label}" class="card" aria-label="{card.label}" on:click={meClicky}>
+<article data-id="{card.label}" class="card" aria-label="{card.label}" on:click={onClick}>
   <header>
     <h3>{card.label}</h3>
     <p class="price">{card.cost}</p>
