9f800175feb4deafc6e3eb7e1c5a07f1c970166a
Tony Grimes <acidtone@tonygrimes.com>
2023-01-21 18:33:06 -0700

    closes #13

--- src/lib/components/PlayerCards.svelte
+++ src/lib/components/PlayerCards.svelte
@@ -1,24 +1,23 @@
 <script>
   import PowerCard from '$lib/components/PowerCard.svelte'
   import PlayerCardMenu from '$lib/components/PlayerCardMenu.svelte'
-  import {gameState} from '../stores/gameState.js';
 
   export let player;
   export let playerIndex;
 
   const handleClick = (index) => {
-    $gameState.players[playerIndex].cards[index].status = 'open';
+    player.cards[index].status = 'open';
   }
 
 </script>
 
 <ul class="player-cards">
-  {#each $gameState.players[playerIndex].cards as card, index}
+  {#each player.cards as card, index}
     <li>
       <!-- svelte-ignore a11y-click-events-have-key-events -->
       <div on:click={() => handleClick(index)}>
         <PowerCard bind:card={player.cards[index]} />
-        {#if $gameState.players[playerIndex].cards[index].status === 'open'}
+        {#if player.cards[index].status === 'open'}
         <PlayerCardMenu cardIndex={index} {playerIndex} bind:player />
         {/if}
       </div>
